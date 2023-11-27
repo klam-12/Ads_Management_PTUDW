a6c978479490614df94a034bb6fd859c91eaaa0e
MinEn1712 <91123918+MinEn1712@users.noreply.github.com>
2023-11-27 22:56:13 +0700

    create a report form template (exclude: save and load data)

---
+++ staticWeb/static/report/reportCard/reportCard.js
@@ -0,0 +1,22 @@
+var reportData = document.querySelectorAll('.report-data-loaded');
+var reportStatus = document.querySelector('.report-status');
+var currentStatus = true;
+
+//TODO: LOADING DATA FROM JSON FILE
+
+//Example
+const loadedData = {
+    reportType : "Quảng cáo thương mại",
+    fullName :"Tran Minh Anh",
+    email : "tranminhanh1912@gmail.com",
+    phoneNumber : "0387400741",
+    reportContent : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+};
+
+const dataValues = Object.values(loadedData);
+for(let i = 0; i < reportData.length; i++){
+    reportData[i].value = dataValues[i];
+};
+
+reportStatus.innerHTML = currentStatus ? "Tình trạng xử lý: Đã xử lý" : "Tình trạng xử lý: Đang xử lý";
+reportStatus.style.color = currentStatus ? '#008000' : '#FF9162';
